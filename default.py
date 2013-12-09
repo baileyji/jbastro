3a4ab9c645f50e318949e2e42c589d23d2e6c7de
Jeb Bailey <baileyji@umich.edu>
2013-12-08 22:10:46 -0500

    Create a default routine for making new python files

---
+++ default.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+
+def parse_cl():
+    parser = argparse.ArgumentParser(description=helpdesc,
+                                     add_help=True)
+    parser.add_argument('-p','--port', dest='PORT',
+                        action='store', required=False, type=int,
+                        help='')
+    parser.add_argument('--log', dest='LOG_LEVEL',
+                        action='store', required=False, default='',
+                        type=str,
+                        help='')
+    return parser.parse_args()
+
+if __name__ =='__main__':
+    args=parse_cl()
