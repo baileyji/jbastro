450cfc66cdd4587e54affeec8ec54f6bd92a0f38
Jeb Bailey <baileyji@umich.edu>
2013-12-15 12:38:09 -0500

    Fix bug in default python file and add fits as a default import

--- default.py
+++ default.py
@@ -2,9 +2,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
+from astropy.io import fits
 
 def parse_cl():
-    parser = argparse.ArgumentParser(description=helpdesc,
+    parser = argparse.ArgumentParser(description='Help undefined',
                                      add_help=True)
     parser.add_argument('-p','--port', dest='PORT',
                         action='store', required=False, type=int,
