bd45b5045a14381ff81bc23937a8aabe3a36b9bd
Jeb Bailey <baileyji@umich.edu>
2014-01-25 21:26:32 -0500

    update the default python file

--- default.py
+++ default.py
@@ -7,13 +7,9 @@ from astropy.io import fits
 def parse_cl():
     parser = argparse.ArgumentParser(description='Help undefined',
                                      add_help=True)
-    parser.add_argument('-p','--port', dest='PORT',
-                        action='store', required=False, type=int,
-                        help='')
-    parser.add_argument('--log', dest='LOG_LEVEL',
-                        action='store', required=False, default='',
-                        type=str,
-                        help='')
+    parser.add_argument('-d','--dir', dest='dir',
+                        action='store', required=False, type=str,
+                        help='',default='./')
     return parser.parse_args()
 
 if __name__ =='__main__':
