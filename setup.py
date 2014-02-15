bf73ae645f18b88c6ccbf0d77ab0dfd1c5bc3fff
Jeb Bailey <baileyji@umich.edu>
2014-02-15 02:17:23 -0300

    First pass at install script

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='jbastro',
+      version='0.1',
+      description='python astro library',
+      author='Jeb Bailey',
+      author_email='baileyji@umich.edu',
+      url='',
+      packages=['jbastro','jbastro.lacosmics'],
+      scripts=[],
+      install_requires=['numpy']
+      )
