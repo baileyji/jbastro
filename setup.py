cc742a7d69bad420982b65c66b20dd6bb2379231
Jeb Bailey <baileyji@umich.edu>
2014-02-16 01:10:11 -0300

    setup.py not installs successfully

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@
 from distutils.core import setup
 
 setup(name='jbastro',
-      version='0.1',
+      version='0.2',
       description='python astro library',
       author='Jeb Bailey',
       author_email='baileyji@umich.edu',
       url='',
       packages=['jbastro','jbastro.lacosmics'],
       scripts=[],
-      install_requires=['numpy']
+      install_requires=['numpy'],
+      package_data={'jbastro':['f2n_fonts/*']}
       )
