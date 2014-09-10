3736e04b47fb55c9f7bc0071be60f57b9de47da0
Jeb Bailey <baileyji@umich.edu>
2014-09-09 22:44:08 -0300

    Add querySkycalc to fetch from ESO SkyCalc

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@
 from distutils.core import setup
 
 setup(name='jbastro',
-      version='0.2',
+      version='0.3',
       description='python astro library',
       author='Jeb Bailey',
       author_email='baileyji@umich.edu',
       url='',
       packages=['jbastro','jbastro.lacosmics'],
       scripts=[],
-      install_requires=['numpy', 'matplotlib'],
+      install_requires=['numpy', 'matplotlib',
+                        'PyEphem','mechanize','BeautifulSoup'],
       package_data={'jbastro':['f2n_fonts/*','data/*']}
       )
